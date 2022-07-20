f7ae4be82179e7793d605d248cea84061a387656
Lora Perrichon <loraperrichon@gmail.com>
2022-07-20 12:08:15 +0200

    CRUD technos ok

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,13 +1,24 @@
 const express = require("express");
 
 const ProjetController = require("./controllers/ProjetController");
+const TechnoController = require("./controllers/TechnoController");
 
 const router = express.Router();
+
+// routes for projects
 
 router.get("/projets", ProjetController.getAll);
 router.get("/projet/:projectId", ProjetController.getOne);
 router.put("/projet/:projectId", ProjetController.updateOne);
 router.post("/projet", ProjetController.createOne);
 router.delete("/projet/:projectId", ProjetController.deleteOne);
+
+// routes for projects
+
+router.get("/technos", TechnoController.getAll);
+router.get("/techno/:technoId", TechnoController.getOne);
+router.put("/techno/:technoId", TechnoController.updateOne);
+router.post("/techno", TechnoController.createOne);
+router.delete("/techno/:technoId", TechnoController.deleteOne);
 
 module.exports = router;
